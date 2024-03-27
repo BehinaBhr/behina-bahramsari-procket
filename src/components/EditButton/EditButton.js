8a6cd7e62bc42c97e9f73b65281b25c9ba37d8b1
BehinaBhr <be.bhr77@gmail.com>
2024-03-26 19:35:31 -0700

    Fix a small bug in EditButton component

--- src/components/EditButton/EditButton.js
+++ src/components/EditButton/EditButton.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import editIcon from "../../assets/images/edit-white-24px.svg";
 import "./EditButton.scss";
 
-function EditButton(to) {
+function EditButton({ to }) {
   return (
     <Link to={to} className="edit-button">
       <img className="edit-button__icon" src={editIcon} alt="circle-edit" />
