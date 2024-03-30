4e310538214f8ed9f0cab9cfc15c87279fc166a2
BehinaBhr <be.bhr77@gmail.com>
2024-03-29 19:01:48 -0700

    Create Add New Task button in goal details page also change the color of all Add buttons

--- src/components/AddButton/AddButton.js
+++ src/components/AddButton/AddButton.js
@@ -2,9 +2,9 @@ import "./AddButton.scss";
 import { Link } from "react-router-dom";
 
 //add a new item
-function AddButton({ target, link_to }) {
+function AddButton({ target, link_to, className = "" }) {
   return (
-    <Link to={link_to}>
+    <Link to={link_to} className={className}>
       <button className="add-button">+ Add New {target}</button>
     </Link>
   );
