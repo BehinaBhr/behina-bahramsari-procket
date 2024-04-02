247940433a5887fbd2e58af9025a1f5344062339
BehinaBhr <be.bhr77@gmail.com>
2024-04-01 21:08:19 -0700

    Add comments to components

--- src/components/ConnectionError/ConnectionError.js
+++ src/components/ConnectionError/ConnectionError.js
@@ -1,6 +1,7 @@
 import "./ConnectionError.scss";
 import errorConnection from"../../assets/images/connection-error.png"
 
+// A connectionError renders on the whole screen in the case of connetection error
 const ConnectionError = ({ error="Unable to access the server right now. Please try again later." }) => {
   return (
     <div className="error">
--- src/components/SuccessfulSubmitMessage/SuccessfulSubmitMessage.js
+++ src/components/SuccessfulSubmitMessage/SuccessfulSubmitMessage.js
@@ -1,5 +1,6 @@
 import "./SuccessfulSubmitMessage.scss";
 
+// Successful form submit message
 const SuccessfulSubmitMessage = ({ message, className = "" }) => {
   return (
     <div className={`successfull-submit-message ${className}`}>
